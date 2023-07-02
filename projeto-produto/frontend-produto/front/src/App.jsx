730c323b60dbeb6c7efb52a86a44ce7b6c847471
Eduardo <103686965+DevEduardoSouza@users.noreply.github.com>
2023-07-02 15:38:48 -0300

    Pequenas alterações

--- projeto-produto/frontend-produto/front/src/App.jsx
+++ projeto-produto/frontend-produto/front/src/App.jsx
@@ -1,33 +1,34 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import Tabela from './Tabela'
-import Formulario from './Formulario'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Tabela from "./Tabela";
+import Formulario from "./Formulario";
 
 function App() {
   //Objeto produto
   const produto = {
     id: 0,
-    name: '',
-    src_image: '',
-    qty: '',
-    marca: '',
-    description: '',
-    price: '',
-    active: 'true'
-  }
+    name: "",
+    src_image: "",
+    qty: "",
+    marca: "",
+    description: "",
+    price: "",
+    active: "true",
+  };
 
   //useState
   const [btnCadastra, setBtnCadastrar] = useState(true);
   const [produtos, setProdutos] = useState([]);
-  const [objProduto, setObjProduto] = useState((produto));
-  const [error, setError] = useState('');
+  const [objProduto, setObjProduto] = useState(produto);
+  const [error, setError] = useState("");
 
+  const [submitted, setSubmitted] = useState(false);
 
   //useEffect -> ele vai fazer a requisição com o beck-end e enviar para o useState
   useEffect(() => {
     fetch("http://localhost:8080/listar")
-      .then(retorno => retorno.json())
-      .then(retorno_convertido => setProdutos(retorno_convertido));
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setProdutos(retorno_convertido));
   }, []);
 
   //Obtendo os dados do formulário
@@ -38,46 +39,44 @@ function App() {
   //Cadastrar produto
   const cadastrar = () => {
     fetch("http://localhost:8080/cadastrar", {
-      method: 'post',
+      method: "post",
       body: JSON.stringify(objProduto),
       headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
     })
-      .then(retorno => retorno.json())
-      .then(retorno_convertido => {
-
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => {
         if (retorno_convertido.message !== undefined) {
           setError(retorno_convertido.message);
+          console.log(retorno_convertido);
         } else {
           //Tenho que adicionar os produtos que já tem com o novo
           setProdutos([...produtos, retorno_convertido]);
-          setError('Cadastro feito como sucesso');
+          setError("Cadastro feito como sucesso");
           limparFormulario();
         }
-      })
-  }
+      });
+  };
 
   //Alterar produto
   const alterar = () => {
     fetch("http://localhost:8080/alterar", {
-      method: 'put',
+      method: "put",
       body: JSON.stringify(objProduto),
       headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
     })
-      .then(retorno => retorno.json())
-      .then(retorno_convertido => {
-
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => {
         if (retorno_convertido.message !== undefined) {
           setError(retorno_convertido.message);
         } else {
           //Mensagem
-          setError('Alteração feita como sucesso');
-
+          setError("Alteração feita como sucesso");
 
           //Cópia do vetor de produtos
           let vetorTemp = [...produtos];
@@ -93,27 +92,23 @@ function App() {
           //Atualizar o vetor de produtos
           setProdutos(vetorTemp);
 
-
-
-
           //Limpar formulário
           limparFormulario();
         }
-      })
-  }
-
+      });
+  };
 
   //Remover produto
   const remover = () => {
     fetch("http://localhost:8080/remover/" + objProduto.id, {
-      method: 'delete',
+      method: "delete",
       headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-type": "application/json",
+        Accept: "application/json",
+      },
     })
-      .then(retorno => retorno.json())
-      .then(retorno_convertido => {
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => {
         //O produto foi removido no banco de dados
         // Agora tenho duas opções atualizar o vetor com uma nova requisição
         // ou remover sem fazer requisição
@@ -121,7 +116,7 @@ function App() {
         //Removi sem fazer requiaição
 
         //Mensagem
-        alert(retorno_convertido.message)
+        alert(retorno_convertido.message);
 
         //Cópia do vetor de produtos
         let vetorTemp = [...produtos];
@@ -139,31 +134,39 @@ function App() {
 
         //Limpar formulário
         limparFormulario();
-
-
-      })
-  }
+      });
+  };
 
   //Limpar formulário
   const limparFormulario = () => {
     setObjProduto(produto);
     setBtnCadastrar(true);
-  }
+  };
 
   //Selecionar produto
 
   const selecionarProduto = (index) => {
     setObjProduto(produtos[index]);
     setBtnCadastrar(false);
-  }
+  };
 
   //Retorno
   return (
     <>
-      <Formulario botao={btnCadastra} evento={aoDigitar} cadastrar={cadastrar} error={error} obj={objProduto} cancelar={limparFormulario} remover={remover}  alterar={alterar}/>
+      <Formulario
+        botao={btnCadastra}
+        evento={aoDigitar}
+        cadastrar={cadastrar}
+        error={error}
+        obj={objProduto}
+        cancelar={limparFormulario}
+        remover={remover}
+        alterar={alterar}
+        validacao={submitted}
+      />
       <Tabela vetor={produtos} selecionar={selecionarProduto} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- projeto-produto/frontend-produto/front/src/Formulario.jsx
+++ projeto-produto/frontend-produto/front/src/Formulario.jsx
@@ -1,46 +1,109 @@
-import React from 'react'
-import './Formulario.css';
+import React from "react";
+import "./Formulario.css";
 
-const Formulario = ({ botao, evento, cadastrar, error, obj , cancelar, remover, alterar}) => {
+const Formulario = ({
+  botao,
+  evento,
+  cadastrar,
+  error,
+  obj,
+  cancelar,
+  remover,
+  alterar,
+}) => {
   return (
     <form>
-      <input type="text" value={obj.name} onChange={evento} name="name" placeholder='Nome' className='form-control' />
-      <input type="text" value={obj.src_image} onChange={evento} name="src_image" placeholder='link da imagem' className='form-control' />
-      <input type="text" value={obj.qty} onChange={evento} name="qty" placeholder='Quantidade' className='form-control' />
-      <input type="text" value={obj.marca} onChange={evento} name="marca" placeholder='Marca' className='form-control' />
-      <input type="text" value={obj.description} onChange={evento} name="description" placeholder='Descrição' className='form-control' />
-      <input type="text" value={obj.price} onChange={evento} name="price" placeholder='Preço' className='form-control' />
+      <input
+        type="text"
+        value={obj.name}
+        onChange={evento}
+        name="name"
+        placeholder="Nome"
+        className="form-control"
+      />
+      <input
+        type="text"
+        value={obj.src_image}
+        onChange={evento}
+        name="src_image"
+        placeholder="link da imagem"
+        className="form-control"
+      />
+      <input
+        type="text"
+        value={obj.qty}
+        onChange={evento}
+        name="qty"
+        placeholder="Quantidade"
+        className="form-control"
+      />
+      <input
+        type="text"
+        value={obj.marca}
+        onChange={evento}
+        name="marca"
+        placeholder="Marca"
+        className="form-control"
+      />
+      <input
+        type="text"
+        value={obj.description}
+        onChange={evento}
+        name="description"
+        placeholder="Descrição"
+        className="form-control"
+      />
+      <input
+        type="text"
+        value={obj.price}
+        onChange={evento}
+        name="price"
+        placeholder="Preço"
+        className="form-control"
+      />
 
-      {
-        error === "Cadastro feito como sucesso" & error !== undefined
-          ?
-          <div className='msgSituacao done'>
-            {error}
-          </div>
-          :
-          <div className='msgSituacao error'>
-            {error}
-          </div>
-      }
-
+      {(error === "Cadastro feito como sucesso") & (error !== undefined) ? (
+        <div className="msgSituacao done">{error}</div>
+      ) : (
+        <div className="msgSituacao error">{error}</div>
+      )}
 
       {/* Buttons */}
       {/* Tenho que fazer um condicional */}
 
-      {
-        botao
-          ? //if
-          <input type="button" value="Cadastrar" className='btn btn-primary' onClick={cadastrar} />
-          : //else if
-          <>
-            <input type="button" value="Alterar" className='btn btn-warning'  onClick={alterar}/>
-            <input type="button" value="Remover" className='btn btn-danger' onClick={remover}/>
-            <input type="button" value="Cencelar" className='btn btn-secondary'  onClick={cancelar}/>
-          </>
-      }
-
+      {botao ? (
+        //if
+        <input
+          type="button"
+          value="Cadastrar"
+          className="btn btn-primary"
+          onClick={cadastrar}
+        />
+      ) : (
+        //else if
+        <>
+          <input
+            type="button"
+            value="Alterar"
+            className="btn btn-warning"
+            onClick={alterar}
+          />
+          <input
+            type="button"
+            value="Remover"
+            className="btn btn-danger"
+            onClick={remover}
+          />
+          <input
+            type="button"
+            value="Cencelar"
+            className="btn btn-secondary"
+            onClick={cancelar}
+          />
+        </>
+      )}
     </form>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
